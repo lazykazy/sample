0e8983aa80d34b0d791f0958db88b53f7ebc9d1a
F.Kazuya <f.kazuya@MacBook-Air.local>
2021-01-04 08:52:17 +0900

    add method

--- sample.rb
+++ sample.rb
@@ -1,91 +1,10 @@
-# case
+def disp_sum(price=1980, num=20)
 
-month = 1
+  return "商品の購入は0以上にしてください:入力値#{num}" if num <= 0
 
-case month
-when 3..5
-  puts "spring"
-when 6..8
-  puts "summer"
-when 9..11
-  puts "fall"
-when 1..12
-  puts "winter"
-else
-  puts "no match"
+  "#{price}円の商品を#{num}個買ったので、合計金額は#{price * num}です"
 end
 
-hp = 11
-puts "yes" if hp >= 10
-puts hp > 10 ? "yes" : "no"
+puts disp_sum(1980, -1)
+puts disp_sum()
 
-puts ""
-puts "-----------------------------"
-
-brave_hp = 30
-brave_attack = 10
-brave_defense = 5
-
-enemy_hp = 30
-enemy_attack = 5
-enemy_defense = 10
-
-select_attack = rand(4)
-
-if select_attack == 0
-  puts "会心の一撃"
-  enemy_damage = brave_attack - enemy_defense + rand(20..30)
-  enemy_hp = enemy_hp - enemy_damage
-else
-  puts "勇者の攻撃"
-  enemy_damage = brave_attack - enemy_defense + rand(3..5)
-  enemy_hp = enemy_hp - enemy_damage
-end
-
-puts <<~TXT
-       敵に#{enemy_damage}のダメージを与えた
-敵の残りHPは#{enemy_hp}だ
-     TXT
-
-if enemy_hp > 20
-  puts "敵は元気だ"
-elsif enemy_hp > 10
-  puts "敵はちょっと弱っている"
-elsif enemy_hp > 5
-  puts "敵はかなり弱っている"
-elsif enemy_hp > 0
-  puts "敵は瀕死だ"
-else
-  puts "敵はしんだ"
-end
-
-puts ""
-
-select_attack = rand(4)
-if select_attack == 0
-  puts "痛恨の一撃"
-  brave_damage = enemy_attack - brave_defense + rand(20..30)
-  brave_hp = brave_hp - brave_damage
-else
-  puts "敵の攻撃"
-  brave_damage = enemy_attack - brave_defense + rand(3..5)
-  brave_hp = brave_hp - brave_damage
-end
-
-puts <<~TXT
-       敵から#{brave_damage}のダメージを受けた。
-ここりHPは#{brave_hp}だ。
-     TXT
-if brave_hp > 20
-  puts "勇者は元気だ"
-elsif brave_hp > 10
-  puts "勇者はちょっと弱っている"
-elsif brave_hp > 5
-  puts "勇者はかなり弱っている"
-elsif brave_hp > 0
-  puts "勇者は瀕死だ"
-else
-  puts "勇者は死んだ"
-end
-
-puts ""
